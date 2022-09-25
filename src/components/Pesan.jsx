8ba63f9e82c2bb91f9dc48fcbf80b518b8c5bd17
Zaid al maghfur <91768583+ZAIDALMAGHFUR@users.noreply.github.com>
2022-09-25 19:58:25 +0700

    zaid

--- src/components/Pesan.jsx
+++ src/components/Pesan.jsx
@@ -9,7 +9,7 @@ function Pesan() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <a href="https://zaid15.my.id" className="flex items-center mb-4 sm:mb-0">
                         <img src={images} alt="" className='mr-3 h-8' />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">Flowbite</span>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">Zaid</span>
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li>
@@ -27,7 +27,7 @@ function Pesan() {
                     </ul>
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" className="hover:underline">Zaid™</a>. All Rights Reserved.
                 </span>
             </footer>
 
